de555814680b7c27714e42ea83011c00bdf6d56f
Nick Brice <nickjbrice98@gmail.com>
2023-05-31 14:24:07 +0100

    git stuff

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Head from "next/head";
 import { TVChartContainer } from "../components/TVChartContainer";
 import {
@@ -34,30 +33,4 @@ export default function GraphPage() {
         </>
     );
 }
-=======
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Simple Web Page</title>
-  <style>
-    body {
-      font-family: Arial, sans-serif;
-      margin: 0;
-      padding: 20px;
-    }
-    h1 {
-      color: #333;
-    }
-    p {
-      color: #666;
-      line-height: 1.5;
-    }
-  </style>
-</head>
-<body>
-  <h1>Welcome to My Web Page</h1>
-  <p>This is a simple web page created using HTML and CSS.</p>
-  <p>Feel free to modify and enhance it as you like!</p>
-</body>
-</html>
->>>>>>> 856058cd4627b583b68ced5893270616cc776d16
+
