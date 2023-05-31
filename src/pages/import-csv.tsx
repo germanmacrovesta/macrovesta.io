e875100bbdfe5939a799b86bdaa5156f6b81816c
Nick Brice <nickjbrice98@gmail.com>
2023-05-31 14:29:49 +0100

    test page for Vic

---
+++ src/pages/import-csv.tsx
@@ -0,0 +1,36 @@
+import Head from "next/head";
+import { TVChartContainer } from "../components/TVChartContainer";
+import {
+    ChartingLibraryWidgetOptions,
+    ResolutionString,
+} from "../../public/static/charting_library/charting_library";
+import ImportCsvForm from '../components/importCSVForm'
+
+// const defaultWidgetProps: Partial<ChartingLibraryWidgetOptions> = {
+//     symbol: "AAPL",
+//     interval: "1D" as ResolutionString,
+//     library_path: "/static/charting_library/",
+//     locale: "en",
+//     charts_storage_url: "https://saveload.tradingview.com",
+//     charts_storage_api_version: "1.1",
+//     client_id: "tradingview.com",
+//     user_id: "public_user_id",
+//     fullscreen: false,
+//     autosize: true,
+// };
+
+export default function GraphPage() {
+    return (
+        <>
+            <Head>
+                <title>TradingView Charting Library and Next.js</title>
+                <script src="/static/datafeeds/udf/dist/bundle.js" async />
+            </Head>
+            <main>
+                {/* <TVChartContainer {...defaultWidgetProps} /> */}
+                <ImportCsvForm />
+            </main>
+        </>
+    );
+}
+
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,34 +1,16 @@
 import Head from "next/head";
-import { TVChartContainer } from "../components/TVChartContainer";
-import {
-    ChartingLibraryWidgetOptions,
-    ResolutionString,
-} from "../../public/static/charting_library/charting_library";
-import ImportCsvForm from '../components/importCSVForm'
+import React from "react";
 
-// const defaultWidgetProps: Partial<ChartingLibraryWidgetOptions> = {
-//     symbol: "AAPL",
-//     interval: "1D" as ResolutionString,
-//     library_path: "/static/charting_library/",
-//     locale: "en",
-//     charts_storage_url: "https://saveload.tradingview.com",
-//     charts_storage_api_version: "1.1",
-//     client_id: "tradingview.com",
-//     user_id: "public_user_id",
-//     fullscreen: false,
-//     autosize: true,
-// };
+
 
 export default function GraphPage() {
     return (
         <>
             <Head>
-                <title>TradingView Charting Library and Next.js</title>
-                <script src="/static/datafeeds/udf/dist/bundle.js" async />
+                <title>Test Page</title>
             </Head>
             <main>
-                {/* <TVChartContainer {...defaultWidgetProps} /> */}
-                <ImportCsvForm />
+                <div>This is a Test Page</div>
             </main>
         </>
     );
